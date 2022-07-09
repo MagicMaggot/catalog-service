b796d4823ec4a0927e12155acff10c3bffb07774
Alexander Ikaev <smea9000@gmail.com>
2022-07-09 22:28:16 +0300

    k8s

--- src/main/kotlin/com/polarbookshop/catalogservice/demo/BookDataLoader.kt
+++ src/main/kotlin/com/polarbookshop/catalogservice/demo/BookDataLoader.kt
@@ -2,14 +2,14 @@ package com.polarbookshop.catalogservice.demo
 
 import com.polarbookshop.catalogservice.domain.Book
 import com.polarbookshop.catalogservice.domain.BookRepository
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.annotation.Profile
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
 @Component
-//@Profile("testdata")
-@ConditionalOnProperty(name = ["polar.testdata"])
+@Profile("testdata")
+//@ConditionalOnProperty(name = ["polar.testdata"])
 class BookDataLoader(private val bookRepository: BookRepository) {
     @EventListener(ApplicationReadyEvent::class)
     fun loadBookTestData() {
